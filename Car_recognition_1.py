38e4cf244baf5900b4738cfd21a8b582b731217d
Andrey Lisitsa <75456617+And-Li@users.noreply.github.com>
2022-02-03 14:24:02 +0800

    Ready model 1.0
    Model ready to be trained
    But it takes some time!!!
    Accuracy on learning data is ok
    Valdiation acuracy is unstable((
    Input datasets need to be reworked

--- Car_recognition_1.py
+++ Car_recognition_1.py
@@ -6,6 +6,7 @@ import numpy as np
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten
 from tensorflow.keras.layers import Dropout, BatchNormalization
+from tensorflow.keras.optimizers import Adam
 
 IMAGE_PATH = 'middle_fmr'
 print(os.listdir(IMAGE_PATH))  # just to be sure you've mounted the right directory
@@ -54,7 +55,6 @@ print(f'The label-dataset-ready-4-learning is shaped like that: {y_data.shape}')
 x_data = x_data/255.  # now everything is ready for the learning!
 
 model = Sequential()
-
 # 1st convolutional layer:
 model.add(Conv2D(256, (3, 3), name='First_C', padding='same', activation='relu',
                  input_shape=(IMG_HEIGHT, IMG_WIDTH, 3)))  # Changed filter quantity (64,128, 3) -> (64, 128, 256)
@@ -80,11 +80,21 @@ model.add(Conv2D(1024, (3,3), name='Sixth_C', padding='same', activation='relu')
 model.add(MaxPooling2D(pool_size=(3, 3), name='Sixth_M'))  # triple compression, with loss (as remaiders r left after division)
                                                            # (7, 14, 1024)-->(2, 4, 1024)
 model.add(Dropout(0.2, name='Sixth_D'))  # unchanged
-
 # Classification layer:
 model.add(Flatten(name='Class_1'))  # all dimensions into 1-dimensional array: (2, 4, 1024)-->(2*4*1024)-->8192
 model.add(Dense(2048, activation='relu', name='Class_2'))  # Dense layer changing neuron quantity 8192-->2048
 model.add(Dense(4096, activation='relu', name='Class_3'))  # Dense layer changing neuron quantity 2048-->4096
 model.add(Dense(CLASS_COUNT, activation='softmax', name='Class_4'))  # output dense layer classifying 4096 into 3 classes
-
 print(model.summary())
+
+model.compile(loss='sparse_categorical_crossentropy',  # used for output of more than 2 classes ('sparce_CE' takes in class labels in 'discrete numbers' view, not in one-hot-encoding)
+              optimizer=Adam(learning_rate=0.0001),
+              metrics=['accuracy'])
+
+store_learning = model.fit(x_data,
+                           y_data,
+                           validation_split=0.2,
+                           shuffle=True,
+                           batch_size=25,
+                           epochs=35,
+                           verbose=1)
